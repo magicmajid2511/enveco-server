cec206b27ee9cfbd576ce0fdf8422cf395102658
Majid Boukachni <boukachni@gmail.com>
2022-09-01 16:31:20 +0200

    adds cors config

--- config/config.js
+++
@@ -1,9 +0,0 @@
-const cors = require("cors")
-
-module.exports = (app) => {
-    app.set("trust proxy", 1)
-    app.use(cors({
-        credentials: true,
-        origin: process.env.ORIGIN || "http://localhost:3000",
-    }))
-}
